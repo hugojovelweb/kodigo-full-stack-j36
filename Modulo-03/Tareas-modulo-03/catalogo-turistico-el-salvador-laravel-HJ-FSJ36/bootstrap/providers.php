c9d93650569248ef4b4465fd2b922b0f0ab9974d
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-08-31 20:07:18 -0600

    Agregando carpeta bootstrap - providers.php

---
+++ Modulo-03/Tareas-modulo-03/catalogo-turistico-el-salvador-laravel-HJ-FSJ36/bootstrap/providers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Hugo Ernesto Jovel Hernandez - FSJ36
+ *
+ * bootstrap/providers.php
+ *
+ * Este archivo ya viene incluido por defecto en cualquier instalación
+ * nueva de Laravel (laravel/laravel). Se incluye aquí únicamente por
+ * completitud, ya que no se han agregado servicios adicionales.
+ */
+
+return [
+    App\Providers\AppServiceProvider::class,
+];
