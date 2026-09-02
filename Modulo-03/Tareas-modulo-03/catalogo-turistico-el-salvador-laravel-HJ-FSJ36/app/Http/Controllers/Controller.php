a97b86140222f07a6d2d645960c6bb4571b37e31
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-09-01 19:49:12 -0600

    Agregando carpeta Http / Controllers - Controller.php

---
+++ Modulo-03/Tareas-modulo-03/catalogo-turistico-el-salvador-laravel-HJ-FSJ36/app/Http/Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Hugo Ernesto Jovel Hernandez - FSJ36
+ * Controlador base de la aplicación.
+ *
+ * Este archivo ya viene incluido por defecto en cualquier instalación
+ * nueva de Laravel (laravel/laravel). Se incluye aquí únicamente por
+ * completitud, ya que LugarController y ContactoController extienden
+ * de esta clase.
+ */
+abstract class Controller
+{
+    //
+}
