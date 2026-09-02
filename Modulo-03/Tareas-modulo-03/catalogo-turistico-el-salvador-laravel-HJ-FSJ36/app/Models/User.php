e3357f755648a373d09bc6928702c223c2ca10c5
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-09-01 19:43:09 -0600

    Agregando carpeta models - User.php

---
+++ Modulo-03/Tareas-modulo-03/catalogo-turistico-el-salvador-laravel-HJ-FSJ36/app/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Hugo Ernesto Jovel Hernandez - FSJ36
+ * 
+ * Modelo Lugar
+ *
+ * En esta práctica NO se utiliza una base de datos relacional ni Eloquent ORM.
+ * Los lugares turísticos se almacenan en un archivo JSON (storage/app/lugares.json)
+ * y este modelo encapsula la lógica de lectura y filtrado de ese archivo.
+ */
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
