430e615551c2d5a9e4d2462bacc563d73e80c12f
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-09-21 23:38:44 -0600

    Agregando carpeta / seeders - DatabaseSeeder.php  en Project- biblioteca-api / Tareas-modulo-03

---
+++ Modulo-03/Tareas-modulo-03/biblioteca-api/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Ejemplo: crear un usuario bibliotecario de prueba.
+        // User::factory()->create([
+        //     'name' => 'Bibliotecario Demo',
+        //     'email' => 'demo@biblioteca.test',
+        // ]);
+    }
+}
